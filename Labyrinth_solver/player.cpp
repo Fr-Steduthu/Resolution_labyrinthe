de4a45fbb8df49e1ded11f8f20c7e3ad39b6ec6b
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2022-12-27 16:37:29 +0100

    position player

--- Labyrinth_solver/player.cpp
+++ Labyrinth_solver/player.cpp
@@ -1,2 +1,33 @@
 #include "pch.h"
 #include "player.h"
+#include <iostream>
+using namespace std;
+
+bool hasnWon = true;
+int hp = 100;
+
+enum Direction = { up, right, left down };
+
+struct Player {
+    char sprite;
+    int x;
+    int y;
+};
+
+    class Player {
+    public:
+        Player player;
+        player.sprite = '@';
+        player.x = 0;
+        player.y = 0;
+        unsigned char maze[10][10];
+        while (hasnWon)
+        {
+            if (maze[player.x][player.y] == ' ') {
+                maze[player.x][player.y] = player.sprite;
+            }
+        }
+
+
+
+    };
