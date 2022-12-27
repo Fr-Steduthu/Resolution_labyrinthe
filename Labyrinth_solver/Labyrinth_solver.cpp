718b2c4bae1b7e1044bb6954ea2b0cfd0fb48a56
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-27 14:50:46 +0100

    Ajout classes demmandées

--- Labyrinth_solver/Labyrinth_solver.cpp
+++ Labyrinth_solver/Labyrinth_solver.cpp
@@ -2,4 +2,3 @@
 
 #include "pch.h"
 #include "framework.h"
-
--- Labyrinth_solver/pch.h
+++ Labyrinth_solver/pch.h
@@ -7,7 +7,10 @@
 #ifndef PCH_H
 #define PCH_H
 
-// add headers that you want to pre-compile here
 #include "framework.h"
+
+#include "grid.h"
+#include "player.h"
+#include "tracer.h"
 
 #endif //PCH_H
