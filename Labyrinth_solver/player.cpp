d4b2cd5a4fc73ac332e3dbb5a7b4cc23ec3111d9
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2022-12-27 16:39:48 +0100

    position player

--- Labyrinth_solver/player.cpp
+++ Labyrinth_solver/player.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool hasnWon = true;
 int hp = 100;
 
-enum Direction = { up, right, left down };
+enum direction = { up, right, left, down };
 
 struct Player {
     char sprite;
@@ -16,6 +16,7 @@ struct Player {
 
     class Player {
     public:
+        int HEIGTH = 21, WIDTH = 31;
         Player player;
         player.sprite = '@';
         player.x = 0;
@@ -25,6 +26,16 @@ struct Player {
         {
             if (maze[player.x][player.y] == ' ') {
                 maze[player.x][player.y] = player.sprite;
+            }
+            else if (maze[player.x][player.y] == '>') {
+                maze[player.y][player.x] == '%';
+            }
+
+            for (int y = 0; y < HEIGTH; y++) {
+                cout << endl;
+                for (int x = 0; x < WIDTH; x++) {
+                    cout << maze[y][x];
+                }
             }
         }
 
