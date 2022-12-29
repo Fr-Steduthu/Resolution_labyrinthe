440c42a15283d794c576c286c1dc27af8e8fdc3e
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-29 14:26:34 +0100

    Update labyrinth.h

--- Labyrinth_solver/labyrinth.h
+++ Labyrinth_solver/labyrinth.h
@@ -1,21 +1,59 @@
-#pragma once*
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include "tracer.h"
+#include "player.h"
+#include "grid.h"
+
 namespace labyrinth
 {
 	class dungeon
 	{
 	public:
 		//Memory
-		dungeon();
+		dungeon(std::ifstream file); // TODO : Le charactere du mur peut possiblement etre determine depuis le fichier, pusique le seul charactere qui n'est pas egal a " "
+		dungeon(std::string data, char wall = '#');
 		~dungeon() = default;
 
 		/**Fonctions du joueur**/
-		void move(const direction& dir);
-		void move_relative(const direction& dir);
+
+		//Returns wether or not the player has been moved
+		bool move(const direction& dir);
+		bool move_relative(const direction& dir);
+
+		/**Operators**/
+		operator std::string() {
+			std::ostringstream s;
+
+			for (int x = 0; x <= this->lab.MAX_X; x++) {
+				for (int y = 0; y <= this->lab.MAX_Y; y++) {
+
+					if(this->character.x == x && this->character.y == y) {
+						s << (this->wall == '#') ? "@" : "#";
+						continue;
+					}
+					
+					s << this->lab.walkable(x, y) ? " " : &this->wall;
+				}
+				s << "\n";
+			}
+
+			return s.str();
+		}
+
+		/***Others**/
+		friend class solvers::solver;
 
 	protected:
 
 	private:
+
 		player character;
 		const grid lab;
+
+		const char wall;
 	};
 }
