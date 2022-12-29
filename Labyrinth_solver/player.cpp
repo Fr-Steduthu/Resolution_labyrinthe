237436a9ec43c461525de363ed6d4433b97016f1
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-29 12:50:48 +0100

    Revert "position player"
    This reverts commit d4b2cd5a4fc73ac332e3dbb5a7b4cc23ec3111d9.

--- Labyrinth_solver/player.cpp
+++ Labyrinth_solver/player.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool hasnWon = true;
 int hp = 100;
 
-enum direction = { up, right, left, down };
+enum Direction = { up, right, left down };
 
 struct Player {
     char sprite;
@@ -16,7 +16,6 @@ struct Player {
 
     class Player {
     public:
-        int HEIGTH = 21, WIDTH = 31;
         Player player;
         player.sprite = '@';
         player.x = 0;
@@ -26,16 +25,6 @@ struct Player {
         {
             if (maze[player.x][player.y] == ' ') {
                 maze[player.x][player.y] = player.sprite;
-            }
-            else if (maze[player.x][player.y] == '>') {
-                maze[player.y][player.x] == '%';
-            }
-
-            for (int y = 0; y < HEIGTH; y++) {
-                cout << endl;
-                for (int x = 0; x < WIDTH; x++) {
-                    cout << maze[y][x];
-                }
             }
         }
 
