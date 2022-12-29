ce9f4db6d1cf25b0ca0db7a3fea0746dbd464f74
Erwann Saint Germain <75694994+Fr-Steduthu@users.noreply.github.com>
2022-12-29 14:26:41 +0100

    Update solver.h

--- Labyrinth_solver/solver.h
+++ Labyrinth_solver/solver.h
@@ -1,27 +1,41 @@
 #pragma once
 
+#include "labyrinth.h"
+#include "grid.h"
+#include "player.h"
+
 namespace labyrinth
 {
 	namespace solvers
 	{
+		enum state { victory, defeat, running, };
+
 		class solver //Classe mere des heuristiques
 		{
 		public:
 
-			solver(std::string lab, const char& wall='#');
-			solver(labyrinth::dungeon);
+			solver(std::string lab, const char& wall = '#') : lab(labyrinth::dungeon(lab, wall)) {};
+			solver(dungeon d) : lab(d) {};
 			virtual ~solver();
 
 			//retrieve_path
 			//resolution_time
-			//display
 
-			virtual void move() = 0; //Code du bot
+			virtual state move() = 0; //Code du bot
+
+			operator std::string() {
+				std::string s(this->lab);
+
+				//TODO
+
+				return s;
+			}
 
 		protected:
+			dungeon lab;
 
-			labyrinth::dungeon lab;
-
+			player& player() { return lab.character; }
+			const grid& grid() { return lab.lab; }
 		private:
 
 		};
